c82661491fb8931f65da22af37a945a90dfd574c
unknown <jiaxiang.hao@bairong.ad.com>
2020-05-06 14:58:49 +0800

    googogog

--- implemented_with_tf/output/graph/wordc.py
+++ implemented_with_tf/output/graph/wordc.py
@@ -62,9 +62,58 @@ def graph(r_path, sw_path):
 
 
 
-if __name__ == "__main__":
-    sw = ['5', '10',  '15']
 
+def get_result(path, s):
+    sample = pd.read_csv(path, encoding='utf-8')
+    print(sample)
+
+    x = list(sample['sentiment'])
+    y = [randint(-100, 100) for i in range(len(x))]
+    n = list(sample['word'])
+
+    return (x, y, n)
+
+    # origin space
+    # md = origin_vec('./雷军评.vec')
+    # print(md)
+    # x = list(md.values())[::50]
+    # y = [1 for i in range(len(x))]
+    # n = list(md.keys())[::50]
+
+    # print(n)
+    # print(x)
+
+
+def graph2(item=None):
+    plt.figure(figsize=(15, 15))
+    plt.plot(item['5'][0], item['5'][1], 'bo', color='red', label='5 seed words', ms=0.001)
+    plt.plot(item['10'][0], item['10'][1], 'bo', color='blue', label='10 seed words', ms=0.001)
+    plt.plot(item['15'][0], item['15'][1], 'bo', color='green', label='15 seed words', ms=0.001)
+    plt.title('The differences of domain specific lexicon with different seed words')
+    plt.legend(loc='best')
+    plt.xlabel('value of domain specific lexicon')
+    clr = {'5':'red', '10':'blue', '15':'green'}
+
+    for it in item:
+        x = item[it][0]
+        y = item[it][1]
+        n = item[it][2]
+        for i, txt in enumerate(n):
+            plt.text(x=x[i], y=y[i], s=txt, fontsize=10, color=clr[it])
+
+    plt.show()
+
+
+
+
+
+if __name__ == "__main__":
+    sw = ['5', '10', '15']
+
+    item = {}
     for s in sw:
-        graph(r_path='../random_video/tot_lexicon'+s+'.csv', 
-              sw_path='../../Utils/source/cn_seed_v2_'+s+'.csv')
+        # graph(r_path='../random_video/tot_lexicon'+s+'.csv', sw_path='../../Utils/source/cn_seed_v2_'+s+'.csv')
+        result = get_result(path='../random_video/tot_lexicon'+s+'.csv', s=s)
+        item[s] = result
+
+    graph2(item=item)
