98c8183d358fdb0decb97d76af5f1c531aca63ae
unknown <jiaxiang.hao@bairong.ad.com>
2020-05-07 22:16:50 +0800

    complete PCA exp

--- implemented_with_tf/output/graph/wordc.py
+++ implemented_with_tf/output/graph/wordc.py
@@ -63,28 +63,43 @@ def graph(r_path, sw_path):
 
 
 
-def get_result(path, s):
+def get_result(path):
     sample = pd.read_csv(path, encoding='utf-8')
-    print(sample)
-
     x = list(sample['sentiment'])
     y = [randint(-100, 100) for i in range(len(x))]
     n = list(sample['word'])
 
     return (x, y, n)
 
-    # origin space
-    # md = origin_vec('./雷军评.vec')
-    # print(md)
-    # x = list(md.values())[::50]
-    # y = [1 for i in range(len(x))]
-    # n = list(md.keys())[::50]
 
-    # print(n)
-    # print(x)
+# load line graph
+def get_result_line(path):
+    sample = pd.read_csv(path, encoding='utf-8')
+    y = sorted(list(sample['sentiment']), reverse=True)
+    x = [i for i in range(len(y))]
+    return (x, y)
 
 
-def graph2(item=None):
+# graph pca
+def graph_pca(item=None):
+    plt.figure(figsize=(15, 15))
+    plt.plot(item['PCA'][0], item['PCA'][1], 'bo', color='red', ms=0.001)
+    plt.title('The lexicon based on PCA')
+    plt.xlabel('principle components')
+    
+    x = item['PCA'][0]
+    y = item['PCA'][1]
+    n = item['PCA'][2]
+
+    for i, txt in enumerate(n):
+        plt.text(x=x[i], y=y[i], s=txt, fontsize=10, color='red')
+
+    plt.show()
+
+
+
+# point graph
+def graph_point(item=None):
     plt.figure(figsize=(15, 15))
     plt.plot(item['5'][0], item['5'][1], 'bo', color='red', label='5 seed words', ms=0.001)
     plt.plot(item['10'][0], item['10'][1], 'bo', color='blue', label='10 seed words', ms=0.001)
@@ -104,16 +119,40 @@ def graph2(item=None):
     plt.show()
 
 
+def graph_line(item=None):
+    plt.figure(figsize=(15, 15))
+    plt.plot(item['5'][0], item['5'][1], '-', color='red', label='5 seed words',)
+    plt.plot(item['10'][0], item['10'][1], '-', color='blue', label='10 seed words')
+    plt.plot(item['15'][0], item['15'][1], '-', color='green', label='15 seed words')
+    
+    plt.legend(loc='best')
+    plt.title('The differences of domain specific lexicon with different seed words')
+    plt.xlabel('value of domain specific lexicon')
+    plt.show()
+
+
 
 
 
 if __name__ == "__main__":
     sw = ['5', '10', '15']
-
     item = {}
-    for s in sw:
-        # graph(r_path='../random_video/tot_lexicon'+s+'.csv', sw_path='../../Utils/source/cn_seed_v2_'+s+'.csv')
-        result = get_result(path='../random_video/tot_lexicon'+s+'.csv', s=s)
-        item[s] = result
+    ##### point #####
+    # for s in sw:
+    #     # graph(r_path='../random_video/tot_lexicon'+s+'.csv', sw_path='../../Utils/source/cn_seed_v2_'+s+'.csv')
+    #     result = get_result(path='../random_video/tot_lexicon'+s+'.csv', s=s)
+    #     item[s] = result
+    # graph_point(item=item)
+    
 
-    graph2(item=item)
+    ##### PCA ######
+    # result = get_result(path='../../../PCA_exp/PCA_result.csv')
+    # item['PCA'] = result
+    # graph_1(item=item)
+
+
+    ##### line ######
+    for s in sw:
+        result = get_result_line(path='../random_video/tot_lexicon'+s+'.csv')
+        item[s] = result
+    graph_line(item=item)
